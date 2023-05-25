449900d8c9e666828051eca5a9200a9801c86976
Shikoqu <Shikoqu@gmail.com>
2023-05-25 12:26:59 +0200

    First test
    added main program and example grammar file

---
+++ Program.py
@@ -0,0 +1,46 @@
+from antlr4 import *
+from grammar.DepotParser import DepotParser
+from grammar.DepotLexer import DepotLexer
+from grammar.DepotParser import DepotParser
+from DepotListener import *
+from Units import *
+
+
+def parse(code: str) -> DepotParser:
+    # Utwórz strumień wejściowy dla analizy składniowej
+    input_stream = InputStream(code)
+
+    # Utwórz lekser
+    lexer = DepotLexer(input_stream)
+    token_stream = CommonTokenStream(lexer)
+
+    # Utwórz parser
+    parser = DepotParser(token_stream)
+    
+    return parser
+
+
+# Przejście przez drzewo rozbioru i przetwarzanie
+def traverse_tree(node, indent=''):
+    if isinstance(node, TerminalNode):
+        print(indent + node.getText())
+    else:
+        for child in node.getChildren():
+            traverse_tree(child, indent + '  ')
+
+
+code = open('example.txt').read()
+
+parser = parse(code)
+tree = parser.program()
+
+# Utwórz instancję listenera
+listener = DepotListener()
+
+# Przechodź przez drzewo rozbioru i wywołuj metody listenera
+walker = ParseTreeWalker()
+walker.walk(listener, tree)
+
+my_depot = listener.data
+
+print(my_depot.save(indent='|'))
